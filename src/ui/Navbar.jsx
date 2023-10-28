6926221d9d524970ebebca9b508883eab398c783
yumelih <yumelih@gmail.com>
2023-10-28 19:05:17 +0300

    board Name bug fixed

--- src/ui/Navbar.jsx
+++ src/ui/Navbar.jsx
@@ -1,16 +1,15 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import SettingsButton from "../features/settings/SettingsButton";
 import Button from "./Button";
-import { openAddTaskModal } from "../features/board/boardSlice";
+import { getBoard, openAddTaskModal } from "../features/board/boardSlice";
 
 function Navbar({ onAddTask }) {
   const dispatch = useDispatch();
+  const boardName = useSelector(getBoard).name;
 
   return (
     <div className="flex items-center bg-secondary px-6 shadow-sm shadow-neutral-50">
-      <h1 className="text-2xl font-semibold text-primaryWhite ">
-        Platform Launch
-      </h1>
+      <h1 className="text-2xl font-semibold text-primaryWhite ">{boardName}</h1>
       <div className="ml-auto mr-2">
         <Button render={() => dispatch(openAddTaskModal(true, "addTaskModal"))}>
           + Add a New Task
